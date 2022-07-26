c9ced225ce07deeb515a769300651a484b6f6349
Maryam <m.b.sarabi@gmail.com>
2022-07-26 05:57:00 -0700

    chang

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -1,2 +1,8 @@
-package MainPackage;public class MainClass {
+package MainPackage;
+public class MainClass {
+    public static void main(String[] args) {
+        SecondClass myobject = new SecondClass();
+        myobject.testMethod();
+
+    }
 }
--- src/MainPackage/SecondClass.java
+++ src/MainPackage/SecondClass.java
@@ -1,2 +1,10 @@
-package MainPackage;public class SecondClass {
+package MainPackage;
+public class SecondClass {
+    int number = 123;
+  //private void testMethod() {
+      // متد بصورت private بوده و قابل دیدن در کلاس مختلف همان پکیچ نمی باشد و SyntaxError میدهد
+    void testMethod() {
+        //متد بصورت package acceses استفاده شده تا در دو کلاس مختلف یک پکیچ قابل اجرا باشد
+        System.out.println(number);
+    }
 }
